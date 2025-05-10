b75e8e21759ad7dfae0bba1fca342e9f57f61eae
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-10 16:47:55 +0900

    画面にhelloが出ることを確認。

--- lib/TFT_Config/User_Setup.h
+++ lib/TFT_Config/User_Setup.h
@@ -1,22 +1,17 @@
-//#define ILI9488_DRIVER
 #define ST7796_DRIVER
 
 #define TFT_WIDTH  320
 #define TFT_HEIGHT 480
 
-#define TFT_MISO -1      // 読み出しなし
-#define TFT_MOSI 23
-#define TFT_SCLK 18
-//#define TFT_CS   15
-#define TFT_CS   14
-#define TFT_DC   2
-#define TFT_RST  -1
-//#define TFT_RST  4
+#define TFT_MISO -1
+#define TFT_MOSI 13    // IO13 = MOSI
+#define TFT_SCLK 14    // IO14 = SCK
+#define TFT_CS   15    // IO15 = CS
+#define TFT_DC   2     // IO2  = DC
+#define TFT_RST  -1    // ENピン接続で制御不要
 
 #define LOAD_GLCD
 #define LOAD_FONT2
 #define LOAD_GFXFF
 
 #define SPI_FREQUENCY  27000000
-//#define SPI_FREQUENCY  20000000
-//#define SPI_FREQUENCY  16000000
