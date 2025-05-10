6035e0e4e610970772a51782dc032656fd39304e
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-10 16:45:57 +0900

    一旦コミット

--- lib/TFT_Config/User_Setup.h
+++ lib/TFT_Config/User_Setup.h
@@ -1,4 +1,5 @@
-#define ILI9488_DRIVER
+//#define ILI9488_DRIVER
+#define ST7796_DRIVER
 
 #define TFT_WIDTH  320
 #define TFT_HEIGHT 480
@@ -6,12 +7,16 @@
 #define TFT_MISO -1      // 読み出しなし
 #define TFT_MOSI 23
 #define TFT_SCLK 18
-#define TFT_CS   15
+//#define TFT_CS   15
+#define TFT_CS   14
 #define TFT_DC   2
-#define TFT_RST  4
+#define TFT_RST  -1
+//#define TFT_RST  4
 
 #define LOAD_GLCD
 #define LOAD_FONT2
 #define LOAD_GFXFF
 
 #define SPI_FREQUENCY  27000000
+//#define SPI_FREQUENCY  20000000
+//#define SPI_FREQUENCY  16000000
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,32 @@
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
+  Serial.begin(115200);
+  delay(500);  // 安定化のための短い待機
+  Serial.println("setup() start");
+
+  pinMode(27, OUTPUT);       // バックライトON
+  digitalWrite(27, HIGH);
+  Serial.println("Backlight ON");
+
   tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setTextSize(2);
+  Serial.println("TFT initialized");
+
+  tft.setRotation(0);
+  Serial.println("Rotation set");
+
+  tft.fillScreen(TFT_RED);
+  Serial.println("Screen filled RED");
+
   tft.setCursor(60, 100);
-  tft.println("Hello World!");
+  tft.setTextSize(2);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.println("Hello!");
+  Serial.println("Text printed");
+
+  Serial.println("setup() complete");
 }
+
 
 void loop() {
 }
