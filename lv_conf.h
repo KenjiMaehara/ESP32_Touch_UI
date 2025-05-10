cc16590100d83991abaa579412d011bc8de1de43
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-10 18:21:13 +0900

    一旦コミット

---
+++ lv_conf.h
@@ -0,0 +1,16 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_COLOR_DEPTH     16
+#define LV_DISP_DEF_REFR_PERIOD 30
+#define LV_HOR_RES_MAX     320
+#define LV_VER_RES_MAX     480
+#define LV_USE_PERF_MONITOR 1
+
+#define LV_USE_LOG 1
+#define LV_LOG_LEVEL LV_LOG_LEVEL_WARN
+
+#define LV_USE_BTN 1
+#define LV_USE_LABEL 1
+
+#endif // LV_CONF_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,64 @@
+#include "../lv_conf.h"   // ← 追加
+#include <lvgl.h>
 #include <TFT_eSPI.h>
+#include <Wire.h>
+
 TFT_eSPI tft = TFT_eSPI();
 
-#define BUTTON_X  60
-#define BUTTON_Y  100
-#define BUTTON_W  200
-#define BUTTON_H  60
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[LV_HOR_RES_MAX * 10];
 
-bool buttonPressed = false;
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
 
-void drawButton(bool pressed) {
-  uint16_t color = pressed ? TFT_GREEN : TFT_BLUE;
-  tft.fillRect(BUTTON_X, BUTTON_Y, BUTTON_W, BUTTON_H, color);
-  tft.drawRect(BUTTON_X, BUTTON_Y, BUTTON_W, BUTTON_H, TFT_WHITE);
-  tft.setTextColor(TFT_WHITE, color);
-  tft.setTextDatum(MC_DATUM);  // 中央揃え
-  tft.drawString("Press Me", BUTTON_X + BUTTON_W / 2, BUTTON_Y + BUTTON_H / 2);
+    tft.startWrite();
+    tft.setAddrWindow(area->x1, area->y1, w, h);
+    tft.pushColors((uint16_t *)&color_p->full, w * h, true);
+    tft.endWrite();
+
+    lv_disp_flush_ready(disp);
+}
+
+static void btn_event_cb(lv_event_t *e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t *btn = lv_event_get_target(e);
+    if (code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+        lv_obj_t *label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Clicked: %d", cnt);
+    }
 }
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(27, OUTPUT);  // バックライトON（必要に応じて変更）
-  digitalWrite(27, HIGH);
+    Serial.begin(115200);
+    tft.begin();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_RED);  // LVGLとは無関係に強制塗り潰し
 
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
+    lv_init();
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, LV_HOR_RES_MAX * 10);
 
-  drawButton(false);  // 初期ボタン表示
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    disp_drv.hor_res = 320;
+    disp_drv.ver_res = 480;
+    lv_disp_drv_register(&disp_drv);
+
+    lv_obj_t *btn = lv_btn_create(lv_scr_act());
+    lv_obj_set_pos(btn, 100, 200);
+    lv_obj_set_size(btn, 120, 50);
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);
+
+    lv_obj_t *label = lv_label_create(btn);
+    lv_label_set_text(label, "Click me!");
+    lv_obj_center(label);
 }
 
 void loop() {
-  // デモのため、自動で色を変える
-  delay(2000);
-  buttonPressed = !buttonPressed;
-  drawButton(buttonPressed);
+    lv_timer_handler(); // LVGLを定期的に実行
+    delay(5);
 }
