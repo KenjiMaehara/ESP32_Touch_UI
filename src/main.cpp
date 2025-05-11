c9e400b5a6658fc94617d80d4d1e6011a6c4f115
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-11 09:39:53 +0900

    ボタン表示OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,45 @@
-#include "../lv_conf.h"   // ← 追加
+
 #include <lvgl.h>
 #include <TFT_eSPI.h>
-#include <Wire.h>
 
 TFT_eSPI tft = TFT_eSPI();
 
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[LV_HOR_RES_MAX * 10];
+#define BACKLIGHT_PIN 27
 
+// バッファサイズ
+static lv_color_t buf[LV_HOR_RES_MAX * 10];
+static lv_disp_draw_buf_t draw_buf;
+
+// 描画フラッシュ関数
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
-    uint32_t w = (area->x2 - area->x1 + 1);
-    uint32_t h = (area->y2 - area->y1 + 1);
+    uint32_t w = area->x2 - area->x1 + 1;
+    uint32_t h = area->y2 - area->y1 + 1;
 
     tft.startWrite();
     tft.setAddrWindow(area->x1, area->y1, w, h);
-    tft.pushColors((uint16_t *)&color_p->full, w * h, true);
+    tft.pushColors((uint16_t *)&color_p->full, w * h, false);  // 同期描画
     tft.endWrite();
 
     lv_disp_flush_ready(disp);
 }
 
-static void btn_event_cb(lv_event_t *e) {
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t *btn = lv_event_get_target(e);
-    if (code == LV_EVENT_CLICKED) {
-        static uint8_t cnt = 0;
-        cnt++;
-        lv_obj_t *label = lv_obj_get_child(btn, 0);
-        lv_label_set_text_fmt(label, "Clicked: %d", cnt);
-    }
-}
-
 void setup() {
     Serial.begin(115200);
 
-    pinMode(27, OUTPUT);
-    digitalWrite(27, HIGH);  // ★ ここがないと画面真っ暗！
+    // バックライトON
+    pinMode(BACKLIGHT_PIN, OUTPUT);
+    digitalWrite(BACKLIGHT_PIN, HIGH);
 
+    // TFT 初期化
     tft.begin();
     tft.setRotation(1);
-    tft.fillScreen(TFT_RED);  // LVGLとは無関係に強制塗り潰し
+    tft.fillScreen(TFT_BLACK);
 
+    // LVGL 初期化
     lv_init();
     lv_disp_draw_buf_init(&draw_buf, buf, NULL, LV_HOR_RES_MAX * 10);
 
+    // LVGLディスプレイドライバ登録
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = my_disp_flush;
@@ -52,10 +48,9 @@ void setup() {
     disp_drv.ver_res = 480;
     lv_disp_drv_register(&disp_drv);
 
+    // ボタン作成
     lv_obj_t *btn = lv_btn_create(lv_scr_act());
-    lv_obj_set_pos(btn, 100, 200);
-    lv_obj_set_size(btn, 120, 50);
-    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn, LV_ALIGN_CENTER, 0, 0);
 
     lv_obj_t *label = lv_label_create(btn);
     lv_label_set_text(label, "Click me!");
@@ -63,6 +58,6 @@ void setup() {
 }
 
 void loop() {
-    lv_timer_handler(); // LVGLを定期的に実行
+    lv_timer_handler();  // LVGL処理ループ
     delay(5);
 }
