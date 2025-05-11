8c45db7dc5aee4cf0bd8a9b771297c24f1143736
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-11 10:12:15 +0900

    一旦コミット。

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,65 @@
 
 #include <lvgl.h>
 #include <TFT_eSPI.h>
+#include <TouchScreen.h>
 
 TFT_eSPI tft = TFT_eSPI();
 
+// バックライト
 #define BACKLIGHT_PIN 27
+
+// 抵抗膜タッチパネルピン定義
+#define XP 27
+#define XM 26
+#define YP 25
+#define YM 33
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 500);
 
 static lv_color_t buf[LV_HOR_RES_MAX * 10];
 static lv_disp_draw_buf_t draw_buf;
 
+// 描画フラッシュ関数
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
     uint32_t w = area->x2 - area->x1 + 1;
     uint32_t h = area->y2 - area->y1 + 1;
 
     tft.startWrite();
     tft.setAddrWindow(area->x1, area->y1, w, h);
-    tft.pushColors((uint16_t *)&color_p->full, w * h, false);  // 同期描画
+    tft.pushColors((uint16_t *)&color_p->full, w * h, false);
     tft.endWrite();
 
     lv_disp_flush_ready(disp);
+}
+
+// タッチ入力取得関数
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
+    TSPoint p = ts.getPoint();
+    if (p.z > 300 && p.z < 1000) {
+        int x = map(p.y, 200, 3800, 0, 480);  // 横型
+        int y = map(p.x, 300, 3700, 0, 320);
+        data->point.x = x;
+        data->point.y = y;
+        data->state = LV_INDEV_STATE_PRESSED;
+    } else {
+        data->state = LV_INDEV_STATE_RELEASED;
+    }
+}
+
+// ボタンイベントコールバック
+void btn_event_cb(lv_event_t *e) {
+    lv_obj_t *label = (lv_obj_t *)lv_event_get_user_data(e);
+    lv_label_set_text(label, "PRESSED!");
 }
 
 void setup() {
     Serial.begin(115200);
 
     pinMode(BACKLIGHT_PIN, OUTPUT);
-    digitalWrite(BACKLIGHT_PIN, HIGH);  // バックライトON
+    digitalWrite(BACKLIGHT_PIN, HIGH);
 
     tft.begin();
-    tft.setRotation(1);  // 横向き（右向き）
+    tft.setRotation(1);  // 横型
     tft.fillScreen(TFT_BLACK);
 
     lv_init();
@@ -38,17 +69,26 @@ void setup() {
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = my_disp_flush;
     disp_drv.draw_buf = &draw_buf;
-    disp_drv.hor_res = 480;  // 横幅
-    disp_drv.ver_res = 320;  // 高さ
+    disp_drv.hor_res = 480;
+    disp_drv.ver_res = 320;
     lv_disp_drv_register(&disp_drv);
 
-    // ボタン作成
+    // 入力デバイス登録
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    // ボタン
     lv_obj_t *btn = lv_btn_create(lv_scr_act());
     lv_obj_align(btn, LV_ALIGN_CENTER, 0, 0);
 
     lv_obj_t *label = lv_label_create(btn);
     lv_label_set_text(label, "Click me!");
     lv_obj_center(label);
+
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_CLICKED, label);
 }
 
 void loop() {
