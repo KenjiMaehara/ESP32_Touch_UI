0da1a1fd0bc1d757a7fc52cf4aa36d0b7976519a
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-11 10:58:09 +0900

    画面表示OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,142 @@
 #include <lvgl.h>
-#include <TFT_eSPI.h>
+#include <LovyanGFX.hpp>
 
-TFT_eSPI tft = TFT_eSPI();
-
-#define BACKLIGHT_PIN 27
-
+static const uint32_t screenWidth  = 320;
+static const uint32_t screenHeight = 480;
 static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[LV_HOR_RES_MAX * 10];
+static lv_color_t buf[2][screenWidth * 10];
+
+class LGFX : public lgfx::LGFX_Device {
+  lgfx::Panel_ST7796  _panel_instance;
+  lgfx::Bus_SPI       _bus_instance;
+  lgfx::Light_PWM     _light_instance;
+  lgfx::Touch_XPT2046 _touch_instance;
+
+public: LGFX(void) {
+    { // SPI bus config
+      auto cfg = _bus_instance.config();
+      cfg.spi_host   = SPI2_HOST;
+      cfg.spi_mode   = 0;
+      cfg.freq_write = 80000000;
+      cfg.freq_read  = 16000000;
+      cfg.spi_3wire  = false;
+      cfg.use_lock   = true;
+      cfg.dma_channel = SPI_DMA_CH_AUTO;
+      cfg.pin_sclk   = 14;
+      cfg.pin_mosi   = 13;
+      cfg.pin_miso   = 12;
+      cfg.pin_dc     = 2;
+      _bus_instance.config(cfg);
+      _panel_instance.setBus(&_bus_instance);
+    }
+    { // panel config
+      auto cfg = _panel_instance.config();
+      cfg.pin_cs = 15;
+      cfg.pin_rst = -1;
+      cfg.pin_busy = -1;
+      cfg.memory_width = screenWidth;
+      cfg.memory_height = screenHeight;
+      cfg.panel_width = screenWidth;
+      cfg.panel_height = screenHeight;
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+      cfg.offset_rotation = 0;
+      cfg.dummy_read_pixel = 8;
+      cfg.dummy_read_bits = 1;
+      cfg.readable = false;
+      cfg.invert = false;
+      cfg.rgb_order = false;
+      cfg.dlen_16bit = false;
+      cfg.bus_shared = false;
+      _panel_instance.config(cfg);
+    }
+    { // backlight config
+      auto cfg = _light_instance.config();
+      cfg.pin_bl = 27;
+      cfg.invert = false;
+      cfg.freq = 44100;
+      cfg.pwm_channel = 7;
+      _light_instance.config(cfg);
+      _panel_instance.setLight(&_light_instance);
+    }
+    { // touch config
+      auto cfg = _touch_instance.config();
+      cfg.x_min = 222;
+      cfg.x_max = 3367;
+      cfg.y_min = 192;
+      cfg.y_max = 3732;
+      cfg.pin_int = -1;
+      cfg.bus_shared = true;
+      cfg.offset_rotation = 6;
+      cfg.spi_host = SPI2_HOST;
+      cfg.freq = 1000000;
+      cfg.pin_sclk = 14;
+      cfg.pin_mosi = 13;
+      cfg.pin_miso = 12;
+      cfg.pin_cs = 33;
+      _touch_instance.config(cfg);
+      _panel_instance.setTouch(&_touch_instance);
+    }
+    setPanel(&_panel_instance);
+  }
+};
+
+LGFX tft;
 
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
-    uint32_t w = area->x2 - area->x1 + 1;
-    uint32_t h = area->y2 - area->y1 + 1;
-    tft.startWrite();
-    tft.setAddrWindow(area->x1, area->y1, w, h);
-    tft.pushColors((uint16_t *)&color_p->full, w * h, true);
-    tft.endWrite();
-    lv_disp_flush_ready(disp);
+  tft.startWrite();
+  tft.setAddrWindow(area->x1, area->y1, area->x2 - area->x1 + 1, area->y2 - area->y1 + 1);
+  tft.pushColors((uint16_t *)&color_p->full, (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1), true);
+  tft.endWrite();
+  lv_disp_flush_ready(disp);
 }
 
 void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
-    uint16_t touchX, touchY;
-    if (tft.getTouch(&touchX, &touchY)) {
-        data->point.x = touchX;
-        data->point.y = touchY;
-        data->state = LV_INDEV_STATE_PRESSED;
-        Serial.printf("Touch: x=%d y=%d\n", touchX, touchY);
-    } else {
-        data->state = LV_INDEV_STATE_RELEASED;
-    }
+  if (tft.getTouch(&data->point.x, &data->point.y)) {
+    data->state = LV_INDEV_STATE_PRESSED;
+  } else {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
 }
 
 void btn_event_cb(lv_event_t *e) {
-    lv_obj_t *label = (lv_obj_t *)lv_event_get_user_data(e);
-    lv_label_set_text(label, "PRESSED!");
+  lv_obj_t *label = (lv_obj_t *)lv_event_get_user_data(e);
+  lv_label_set_text(label, "PRESSED!");
 }
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(BACKLIGHT_PIN, OUTPUT);
-    digitalWrite(BACKLIGHT_PIN, HIGH);
+  Serial.begin(115200);
+  tft.init();
+  tft.setRotation(1);
 
-    tft.begin();
-    tft.setRotation(1);
-    tft.fillScreen(TFT_BLACK);
+  lv_init();
+  lv_disp_draw_buf_init(&draw_buf, buf[0], buf[1], screenWidth * 10);
 
-    lv_init();
-    lv_disp_draw_buf_init(&draw_buf, buf, NULL, LV_HOR_RES_MAX * 10);
+  static lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  lv_disp_drv_register(&disp_drv);
 
-    static lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    disp_drv.flush_cb = my_disp_flush;
-    disp_drv.draw_buf = &draw_buf;
-    disp_drv.hor_res = 480;
-    disp_drv.ver_res = 320;
-    lv_disp_drv_register(&disp_drv);
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
 
-    static lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = my_touchpad_read;
-    lv_indev_drv_register(&indev_drv);
+  lv_obj_t *btn = lv_btn_create(lv_scr_act());
+  lv_obj_align(btn, LV_ALIGN_CENTER, 0, 0);
 
-    lv_obj_t *btn = lv_btn_create(lv_scr_act());
-    lv_obj_align(btn, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_t *label = lv_label_create(btn);
+  lv_label_set_text(label, "Click me!");
+  lv_obj_center(label);
 
-    lv_obj_t *label = lv_label_create(btn);
-    lv_label_set_text(label, "Click me!");
-    lv_obj_center(label);
-
-    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_CLICKED, label);
+  lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_CLICKED, label);
 }
 
 void loop() {
-    lv_timer_handler();
-    delay(5);
+  lv_timer_handler();
+  delay(5);
 }
