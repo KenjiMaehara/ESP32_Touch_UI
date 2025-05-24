0e47ecc11f8e82b09e2d5569a025e8f15aa85ffc
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-25 04:02:01 +0900

    一旦コミット

--- src/clock_page.cpp
+++ src/clock_page.cpp
@@ -2,7 +2,7 @@
 #include "clock_page.h"
 #include "MontserratBold64.h"  // フォントデータを直接インクルード
 
-extern const lv_font_t lv_font_montserrat_64;
+extern const lv_font_t MontserratBold64;
 
 lv_obj_t* clock_label = nullptr;
 extern void create_color_button_screen();
@@ -22,14 +22,18 @@ void create_clock_screen() {
     // ラベルラッパー
     lv_obj_t *wrapper = lv_obj_create(lv_scr_act());
     lv_obj_remove_style_all(wrapper);
-    lv_obj_set_size(wrapper, 300, 120);
-    lv_obj_align(wrapper, LV_ALIGN_CENTER, 0, -50);
+    lv_obj_set_size(wrapper, 300, 120);  // 高さを広げてフォントがはみ出ないようにする
+    lv_obj_align(wrapper, LV_ALIGN_CENTER, 0, -50);  // 上に配置
 
     // ラベル本体
     clock_label = lv_label_create(wrapper);
     lv_label_set_text(clock_label, "00:00");
-    lv_obj_set_style_text_font(clock_label, &MontserratBold64, 0);  // 修正済み
+    lv_obj_set_style_text_font(clock_label, &MontserratBold64, 0);
     lv_obj_center(clock_label);
+
+    // 表示確認用にボーダー追加（テスト用、不要なら削除）
+    lv_obj_set_style_border_width(clock_label, 2, 0);
+    lv_obj_set_style_border_color(clock_label, lv_color_hex(0xFF0000), 0);
 
     // Nextボタン
     lv_obj_t *next_btn = lv_btn_create(lv_scr_act());
