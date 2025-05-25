253b65ac7c8e8e8b7b04c961c0a8dd27ede77c01
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-25 19:45:07 +0900

    一旦コミット

--- src/LGFX_ESP32_ILI9488.hpp
+++ src/LGFX_ESP32_ILI9488.hpp
@@ -2,20 +2,18 @@
 #include <LovyanGFX.hpp>
 
 class MyLGFX : public lgfx::LGFX_Device {
-  // SPIバス設定
-  lgfx::Bus_SPI _bus_instance;
-
-  // パネルとタッチをnewで動的生成（v0.4.7仕様に適合）
-  lgfx::Panel_ILI9488* _panel_instance = nullptr;
-  lgfx::Touch_XPT2046* _touch_instance = nullptr;
+  lgfx::v0::Panel_ILI9488* _panel;
+  lgfx::v0::Bus_SPI* _bus;
+  lgfx::v0::Touch_XPT2046* _touch;
 
 public:
-  LGFX() {
-    _panel_instance = new lgfx::Panel_ILI9488();
-    _touch_instance = new lgfx::Touch_XPT2046();
+  MyLGFX() {
+    _panel = new lgfx::v0::Panel_ILI9488();
+    _bus = new lgfx::v0::Bus_SPI();
+    _touch = new lgfx::v0::Touch_XPT2046();
 
     { // SPIバス設定
-      auto cfg = _bus_instance.config();
+      auto cfg = _bus->config();
       cfg.spi_host = VSPI_HOST;
       cfg.spi_mode = 0;
       cfg.freq_write = 40000000;
@@ -30,16 +28,14 @@ public:
       cfg.pin_cs   = 15;
       cfg.pin_rst  = 4;
       cfg.pin_busy = -1;
-      _bus_instance.config(cfg);
-      _panel_instance->setBus(&_bus_instance);
+      _bus->config(cfg);
+      _panel->setBus(_bus);
     }
 
     { // パネル設定
-      auto cfg = _panel_instance->config();
-      cfg.panel_width  = 320;
+      auto cfg = _panel->config();
+      cfg.panel_width = 320;
       cfg.panel_height = 480;
-      cfg.offset_x = 0;
-      cfg.offset_y = 0;
       cfg.offset_rotation = 0;
       cfg.dummy_read_pixel = 8;
       cfg.dummy_read_bits  = 1;
@@ -48,11 +44,11 @@ public:
       cfg.rgb_order = false;
       cfg.dlen_16bit = false;
       cfg.bus_shared = true;
-      _panel_instance->config(cfg);
+      _panel->config(cfg);
     }
 
-    { // タッチ設定
-      auto cfg = _touch_instance->config();
+    { // タッチパネル設定
+      auto cfg = _touch->config();
       cfg.x_min = 200;
       cfg.x_max = 3900;
       cfg.y_min = 200;
@@ -66,10 +62,10 @@ public:
       cfg.pin_mosi = 23;
       cfg.pin_miso = 19;
       cfg.pin_cs   = 33;
-      _touch_instance->config(cfg);
-      _panel_instance->setTouch(_touch_instance);
+      _touch->config(cfg);
+      _panel->setTouch(_touch);
     }
 
-    setPanel(_panel_instance);  // ここが v0.4.7 向けに正しい呼び出し
+    setPanel(_panel);  // これで純粋仮想関数が満たされる
   }
 };
