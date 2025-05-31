1b9a0db9b164af04317cb93a5c32896b082d0dc7
KenjiMaehara <kmaehara@mel.co.jp>
2025-05-31 19:33:34 +0900

    一旦コミット

--- src/clock_page.cpp
+++ src/clock_page.cpp
@@ -12,14 +12,14 @@ void create_clock_screen() {
     // ラベルラッパー
     lv_obj_t* wrapper = lv_obj_create(lv_scr_act());
     lv_obj_remove_style_all(wrapper);
-    lv_obj_set_size(wrapper, 480, 240);  // 画面いっぱいに確保
+    lv_obj_set_size(wrapper, 480, 320);  // 画面いっぱいに確保
     lv_obj_align(wrapper, LV_ALIGN_CENTER, 0, 0);
 
     // 時計ラベル
     clock_label = lv_label_create(wrapper);
-    lv_label_set_text(clock_label, "00:00");
     lv_obj_set_style_text_font(clock_label, &MontserratBold64, 0);
-    lv_obj_set_style_text_color(clock_label, lv_color_hex(0xFFFFFF), 0); // 白文字
+    lv_obj_set_style_text_color(clock_label, lv_color_hex(0xFFFFFF), 0);
+    lv_label_set_text(clock_label, "00:00");
     lv_obj_center(clock_label);
 
     // デバッグ用にラベル枠を表示
