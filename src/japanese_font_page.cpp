b2550e96763f8d1f5dbbfebef04e7afef7b82382
KenjiMaehara <kmaehara@mel.co.jp>
2025-06-01 18:17:29 +0900

    一旦コミット

--- src/japanese_font_page.cpp
+++ src/japanese_font_page.cpp
@@ -9,10 +9,10 @@ void create_japanese_font_screen() {
     lv_obj_t *label = lv_label_create(scr);
 
     // テキスト設定（日本語）
-    lv_label_set_text(label, "こんにちは 世界\n時刻 設定");
+    lv_label_set_text(label, "こんにちは 世界 時刻 設定");
 
     // フォント設定
-    lv_obj_set_style_text_font(label, &NotoSansJPRegular32, LV_PART_MAIN);
+    //lv_obj_set_style_text_font(label, &NotoSansJPRegular32, LV_PART_MAIN);
 
     // テキスト中央揃え
     lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
