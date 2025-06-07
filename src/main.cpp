8cf449b46611228768ae0444a38024ef21ab2ce7
KenjiMaehara <kmaehara@mel.co.jp>
2025-06-07 14:32:58 +0900

    一旦コミット

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,28 @@
 
 class LGFX : public lgfx::LGFX_Device {
 public:
-  lgfx::Panel_ILI9488 _panel;
-  lgfx::Bus_SPI _bus;
+  lgfx::Panel_ILI9488_8080 _panel;
+  lgfx::Bus_Parallel8 _bus;
 
   LGFX(void) {
     {
       auto cfg = _bus.config();
-      cfg.spi_host = SPI2_HOST;
-      cfg.spi_mode = 0;
-      cfg.freq_write = 40000000;
-      cfg.freq_read  = 16000000;
-      cfg.spi_3wire = false;
-      cfg.use_lock = true;
-      cfg.dma_channel = 1;
-      cfg.pin_sclk = 14;
-      cfg.pin_mosi = 13;
-      cfg.pin_miso = 12;
-      cfg.pin_dc   = 2;
+      cfg.port = 0;
+      cfg.freq_write = 20000000;
+      cfg.pin_wr = 4;     // WRX
+      cfg.pin_rd = -1;    // RD (未使用)
+      cfg.pin_rs = 2;     // DCX
+
+      // データバス（DB0～DB7）
+      cfg.pin_d0 = 32;
+      cfg.pin_d1 = 33;
+      cfg.pin_d2 = 25;
+      cfg.pin_d3 = 26;
+      cfg.pin_d4 = 27;
+      cfg.pin_d5 = 14;
+      cfg.pin_d6 = 12;
+      cfg.pin_d7 = 13;
+
       _bus.config(cfg);
       _panel.setBus(&_bus);
     }
@@ -28,19 +33,22 @@ public:
       cfg.pin_cs = 15;
       cfg.pin_rst = -1;
       cfg.pin_busy = -1;
-      cfg.memory_width  = 320;
+      cfg.memory_width = 320;
       cfg.memory_height = 480;
-      cfg.panel_width   = 320;
-      cfg.panel_height  = 480;
+      cfg.panel_width = 320;
+      cfg.panel_height = 480;
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+      cfg.offset_rotation = 0;
       cfg.invert = false;
       cfg.rgb_order = false;
       cfg.dlen_16bit = false;
-      cfg.bus_shared = true;
+      cfg.bus_shared = false;
       _panel.config(cfg);
     }
 
+    _panel.setLight(nullptr); // バックライト常時ON
     setPanel(&_panel);
-    _panel.setLight(nullptr);  // ← バックライト制御なしで強制ON
   }
 };
 
@@ -51,10 +59,12 @@ void setup() {
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_RED);
+  tft.setTextColor(TFT_GREEN);
   tft.setTextSize(2);
   tft.setCursor(80, 120);
-  tft.print("HELLO TEST");
+  tft.print("Hello, World!");
 }
 
-void loop() {}
+void loop() {
+  // 何もしない
+}
