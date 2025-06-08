6dc1814da70f6f548586176977f0c5487dc8c7e9
KenjiMaehara <kmaehara@mel.co.jp>
2025-06-08 17:40:34 +0900

    一旦コミット

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <LovyanGFX.hpp>
+#include <functional>
 
 class LGFX : public lgfx::LGFX_Device {
   lgfx::Panel_ST7796 _panel;
@@ -78,7 +79,6 @@ public:
   }
 };
 
-// --- LGFX クラス定義の後に配置した MyButton クラス ---
 class MyButton {
 public:
   int16_t x, y, w, h;
@@ -86,6 +86,7 @@ public:
   const char* label;
   uint8_t textsize;
   LGFX* gfx;
+  std::function<void()> callback;
 
   void initButton(LGFX* gfx_, int16_t x_, int16_t y_, int16_t w_, int16_t h_, uint16_t outline, uint16_t fill, uint16_t text, const char* label_, uint8_t textsize_) {
     gfx = gfx_;
@@ -98,6 +99,14 @@ public:
     textcolor = text;
     label = label_;
     textsize = textsize_;
+  }
+
+  void setCallback(std::function<void()> cb) {
+    callback = cb;
+  }
+
+  void trigger() {
+    if (callback) callback();
   }
 
   void drawButton(bool inverted = false) {
@@ -125,15 +134,12 @@ lgfx::touch_point_t tp;
 String input_value = "";
 MyButton nextButton;
 
-static const char* keys[4][3] = {
-  {"1", "2", "3"},
-  {"4", "5", "6"},
-  {"7", "8", "9"},
-  {"*", "0", "#"}
-};
-
 void drawNextButton(int x, int y, int w, int h) {
   nextButton.initButton(&tft, x + w/2, y + h/2, w, h, TFT_WHITE, TFT_GREEN, TFT_BLACK, "Next", 2);
+  nextButton.setCallback([]() {
+    screen_state = (screen_state + 1) % total_screens;
+    showCurrentScreen();
+  });
   nextButton.drawButton();
 }
 
@@ -171,18 +177,6 @@ void showScreen3() {
   tft.setCursor(40, 20);
   tft.print("Input: " + input_value);
 
-  for (int row = 0; row < 4; row++) {
-    for (int col = 0; col < 3; col++) {
-      int x = 40 + col * 80;
-      int y = 40 + row * 70;
-      tft.fillRect(x, y, 60, 60, TFT_WHITE);
-      tft.setTextColor(TFT_BLACK);
-      tft.setTextSize(3);
-      tft.setCursor(x + 20, y + 15);
-      tft.print(keys[row][col]);
-    }
-  }
-
   drawNextButton(240, 260, 60, 40);
 }
 
@@ -206,31 +200,9 @@ void setup() {
 void loop() {
   if (tft.getTouch(&tp)) {
     Serial.printf("Touch: x=%d y=%d\n", tp.x, tp.y);
-
-    if (screen_state < 3 && nextButton.contains(tp.x, tp.y)) {
-      screen_state = (screen_state + 1) % total_screens;
-      showCurrentScreen();
+    if (nextButton.contains(tp.x, tp.y)) {
+      nextButton.trigger();
       delay(300);
-    }
-    else if (screen_state == 3) {
-      for (int row = 0; row < 4; row++) {
-        for (int col = 0; col < 3; col++) {
-          int x = 40 + col * 80;
-          int y = 40 + row * 70;
-          if (tp.x > x && tp.x < x + 60 && tp.y > y && tp.y < y + 60) {
-            input_value += keys[row][col];
-            showScreen3();
-            delay(300);
-            return;
-          }
-        }
-      }
-      if (nextButton.contains(tp.x, tp.y)) {
-        screen_state = 0;
-        showCurrentScreen();
-        delay(300);
-        return;
-      }
     }
   }
 }
